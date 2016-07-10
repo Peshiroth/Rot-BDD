9b2e2c7a0f5b92e59013fe3cb12300f5f5aa4d78
lgkinter <lgkinter@yahoo.com>
2016-07-10 16:11:43 -0400

    Complete filter_movie_list feature tests

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,35 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create!(movie)
+  end
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  fail "Unimplemented"
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(", ").each do |rating|
+    if uncheck
+      step %Q{I uncheck "ratings[#{rating}]"}
+    else
+      step %Q{I check "ratings[#{rating}]"}
+    end
+  end
+end
+
+Then /I should see all the movies/ do
+  rows = page.all('tbody tr').count
+  expect(rows).to eq Movie.count
+end
